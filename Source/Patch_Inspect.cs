6991ac39024590543109846242db42939e24846c
kathanon <cower@cower.se>
2023-08-12 12:37:04 +0200

    Released version.

--- Source/Patch_Inspect.cs
+++ Source/Patch_Inspect.cs
@@ -10,13 +10,6 @@ using Verse;
 namespace WhoIsUsingThat;
 [HarmonyPatch]
 public static class Patch_Inspect {
-    private static readonly string[] DescPatterns = {
-        "Reserved by {0}",
-        "Reserved by {0} for {1}",
-        "{2} reserved by {0}",
-        "{2} reserved by {0} for {1}",
-    };
-
     [HarmonyPostfix]
     [HarmonyPatch(typeof(Thing), nameof(Thing.GetInspectString))]
     public static void GetInspectString(Thing __instance, ref string __result) {
@@ -34,11 +27,10 @@ public static class Patch_Inspect {
         var name = pawn.NameShortColored;
         Regex.Escape(self.Label);
         string job = res.Job?.GetReport(pawn)?.Trim();
-        string pattern = "([ ]+|(?<![a-zA-Z0-9]))" + Regex.Escape(self.LabelNoCount) + "( x[0-9]+)?([ ]+|(?![a-zA-Z0-9]))";
+        string pattern = "[ ]*\\b" + Regex.Escape(self.LabelNoCount) + "( x[0-9]+)?\\b[ ]*";
         job = Regex.Replace(job, pattern, " ", RegexOptions.IgnoreCase);
         int count = (res.StackCount < self.stackCount) ? res.StackCount : 0;
-        int i = ((job != null) ? 1 : 0) + ((count != 0) ? 2 : 0);
-        var desc = string.Format(DescPatterns[i], name, job, count);
+        var desc = Strings.AddedDescription(name, job, count);
         __result = (__result.NullOrEmpty()) ? desc : $"{__result}\n{desc}";
     }
 }
--- Source/Strings.cs
+++ Source/Strings.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace WhoIsUsingThat {
     public static class Strings {
         public const string ID = "kathanon.WhoIsUsingThat";
         public const string Name = "Who's Using That?";
+
+        private static readonly string AddedDescriptionKeyFormat = 
+            "kathanon.WhoIsUsingThat.AddedDescription.Job{0}.Count{1}";
+
+        public static string AddedDescription(string name, string job, int count) 
+            => string.Format(AddedDescriptionKeyFormat, (job != null), (count > 0))
+                     .Translate(name, job, count);
     }
 }
